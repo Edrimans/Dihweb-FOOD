cc3724fa1dfa386dc8e7dbd06a316f5bfd8e528f
Edrimans <tipsydrunkk@gmail.com>
2025-03-28 14:46:38 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,45 @@
+<footer>
+    <div class="footer_main">
+        <div class="footer_tag">
+            <h2>Location</h2>
+            <p>Sri Lanka</p>
+            <p>USA</p>
+            <p>India</p>
+            <p>Japan</p>
+            <p>Italy</p>
+        </div>
+
+        <div class="footer_tag">
+            <h2>Quick Link</h2>
+            <a href="index.php"><p>Home</p></a>
+            <a href="about.php"><p>About</p></a>
+            <a href="menu.php"><p>Menu</p></a>
+            <a href="menu.php"><p>Order</p></a>
+        </div>
+
+        <div class="footer_tag">
+            <h2>Contact</h2>
+            <p>+64 981 028 7858</p>
+            <p>+64 993 525 3551</p>
+            <p>ochoco.calvin@gmail.com</p>
+            <p>food@gmail.com</p>
+        </div>
+
+        <div class="footer_tag">
+            <h2>Our Services</h2>
+            <p>Fast Delivery</p>
+            <p>Easy Payments</p>
+            <p>24 x 7 Service</p>
+        </div>
+
+        <div class="footer_tag">
+            <h2>Follows</h2>
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-linkedin-in"></i>
+        </div>
+    </div>
+
+    <p class="end">Design by <span><i class="fa-solid fa-face-grin"></i> Calvin Ochoco</span></p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Website</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="short icon" href="image/short_icon.png">
+</head>
+
+    <!--Navigation Bar-->
+
+    <nav>
+
+        <div class="logo">
+            <a href="index.php"><img src="image/logo.png"></a>
+        </div>
+
+        <ul>
+            <li><a href="index.php" ><b>Home</b></a></li>
+            <li><a href="menu.php"><b>Menu</b></a></li>
+            <li><a href="about.php"><b>About</b></a></li>
+            <li><a href="blog.php"><b>Reviews</b></a></li>
+        </ul>
+
+
+    </nav>
